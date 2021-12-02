12c77aaf3a4ae0a5ba40b809f09f0f26e2d31765
FannyLourioux <f.lourioux@gmail.com>
2021-12-02 21:51:15 +0100

    style: modif home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,35 +1,34 @@
-import { Grid, Typography} from '@material-ui/core';
+import { Grid, Paper, Typography, Box } from '@material-ui/core';
 import React from 'react';
-import Layout from '../../components/Layout/Layout';
 
 import './HomePage.css';
 
 const HomePage = () => {
   return (
     <>
-      <Layout
-        page="Home"
-        blockBody = {
-          <React.Fragment>
-            <Grid container>
-              <Grid item>
-                <Typography className='Title' variant='h1' key="State">Bienvenue sur le sauvetage de l'équipe Serval</Typography>
-              </Grid>
-              <Grid item >
-                <img alt=" " className="home__image" src="serval_software.png" style={{ width: 896, height: 399 }}/>
-              </Grid>
+    <Paper elevation={0}>
+      <Typography component="div" style={{ backgroundColor: '#C2D3F7', paddingLeft:0 ,paddingTop:5}}>
+        <Box sx={{ minHeight: '100vh', py: 3, margin:0 }}>
+          <Grid container spacing="2" className="grid_home">
+            <Grid item xs="4">
+              <img alt=" " className="home__image" src="serval_software.png"/>
             </Grid>
-            <Grid container direction="column" justifyContent="center">
-              <Grid item xs="3">
-                <Typography variant='subtitle2'>A l'aide!! L'équipe Serval est parti en mer pour recourir un bateau déchu!!</Typography>
-              </Grid>
-              <Grid item xs="3">
-                <Typography variant='subtitle2'>Partez à la bataille et remporter les mini-jeux pour sauver les marins!!</Typography>
-              </Grid>
+            <Grid item xs="7">
+              <Typography className='Title' variant='h2' key="State">Bienvenue sur le sauvetage de l'équipe Serval</Typography>
             </Grid>
-          </React.Fragment>
-        }
-         />   
+          </Grid>
+          <Grid container direction="column" justifyContent="center">
+            <Grid item>
+              <Typography variant='subtitle'>A l'aide!! L'équipe Serval est parti en mer pour secourir un bateau déchu !!</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='subtitle'>Partez à la bataille et remportez les mini-jeux pour sauver les marins !!</Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Typography>
+    </Paper>
+      
       
     </>
   );
