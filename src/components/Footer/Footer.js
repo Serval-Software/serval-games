7d684936eec872e03294b6779b65684475a5c0d1
FannyLourioux <f.lourioux@gmail.com>
2021-12-03 03:58:29 +0100

    feat: add shark attack game

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,15 @@ import "./Footer.css";
 const Footer = () => {
     return (
 
-                <Grid className="footer">
-                    <div>Icônes conçues par <a href="https://www.flaticon.com/fr/auteurs/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/fr/" title="Flaticon">www.flaticon.com</a></div>
-                    <div>Icônes conçues par <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/fr/" title="Flaticon">www.flaticon.com</a></div>
-                    <div> </div>
+                <Grid className="footer" container direction="row" alignItems="flex" >
+                    <Grid item xs={8}>
+                        <div>Icônes conçues par <a href="https://www.flaticon.com/fr/auteurs/victoruler" title="Victoruler">Victoruler</a> from <a href="https://www.flaticon.com/fr/" title="Flaticon">www.flaticon.com</a></div>
+                        <div>Icônes conçues par <a href="https://www.flaticon.com/fr/auteurs/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/fr/" title="Flaticon">www.flaticon.com</a></div>
+                        <div>Icons made by <a href="https://www.flaticon.com/authors/catkuro" title="catkuro">catkuro</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                    </Grid>
+                    <Grid item xs={4}>
+                        Ce site est la propriété de Serval Software <img alt=" " src="/serval.png" className="logo" id="logo-footer" height="50" width="50"/>
+                    </Grid>
                 </Grid>
 
         
