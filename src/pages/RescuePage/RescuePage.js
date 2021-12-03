ae4f8f9c530a69c724bbedcb946abbbc529d0213
Trystan Roches <trystan.rcs4@gmail.com>
2021-12-03 04:14:14 +0100

    feat: front RescuePage

--- src/pages/RescuePage/RescuePage.js
+++ src/pages/RescuePage/RescuePage.js
@@ -1,15 +1,60 @@
-import { Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
 
 const RescuePage = () => {
+
+  const [yearText, setYearText] = useState({});
+
+  useEffect(() => {
+
+    axios.post("https://localhost:8080/scrap")
+    .then((response)=> {
+      if(response.data !== null) setYearText(response.data);
+    })
+
+
+  })
+
+  function afficherTexte() {
+    return(
+      Object.entries(yearText).map(([key, value]) =>{
+        Object.entries(value).map(([key_, leTableau]) =>{
+          const {year, text} = leTableau;
+            return(
+              <>
+                <Grid item>
+                  {
+                    year
+                  }
+                </Grid>
+                <Grid item>
+                  {
+                    text
+                  }
+                </Grid>
+              </>
+            )
+        })
+      })
+    )
+    
+  }
+
+
   return (
     <>
       <Layout
           page="Home"
           blockBody = {
+            <>
             <Typography variant='h4'>WikiRescue</Typography>
-
+            <Grid container direction="column" alignItems="center">
+              {
+                afficherTexte()
+              }
+            </Grid>
+            </>
           }
         />
     </>
